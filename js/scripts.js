1ef5df77ea0b1100e1393d2d41520386238a8a4d
John Parsons <john.parsons85210@gmail.com>
2017-08-22 16:44:36 -0700

    add backend functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,44 @@
-//business logic
-function Ticket(title, date, time, amount) {
-  this.movieName= title;
-  this.movieDate = date;
-  this.movieTime = time;
-  this.ticketAmount = [];
-}
+$(document).ready(function() {
+$("p").hide();
+var numberOfTickets= document.querySelector('#amount').value;
+console.log(numberOfTickets);
 
-function Address(street, city, state) {
-  this.street = street;
-  this.city = city;
-  this.state = state;
-}
+var movieTicket = {
+  nameOfMovie: function (title) {
+    this.title = title
+  },
+  cost: function(price) {
+    this.price = price
+  },
+  purchase: function(){
+    $("<p>"+"You have purchased "  +numberOfTickets+' ticket(s) to ' +this.title+"</p>" ).appendTo('.ticketText');
+    $("<p>" +'The cost will be $' + this.price*(numberOfTickets)+"</p>").appendTo('.ticketText')
+ }
+};
 
-Ticket.prototype.fullName = function() {
-  return this.movieName + " on " + this.movieDate + " at " + this.movieTime;
-}
+shrek = Object.create(movieTicket);
+shrek.nameOfMovie('Shrek 9')
+shrek.cost(15)
+
+hipster = Object.create(movieTicket)
+hipster.nameOfMovie('Hipster Garbage')
+hipster.cost(25)
+// hipster.purchase()
+
+cruise = Object.create(movieTicket)
+cruise.nameOfMovie("Tom Cruise: The Autobiopic")
+cruise.cost(15)
+// cruise.purchase()
+
+$("#show").click(function(){
+    $("p").show();
+    shrek.purchase()
+});
+});
+
+
+
+// $("form#button").submit(function(event) {
+//   alert('working!');
+//
+//
